d42f3d89e5046d3c84a013bdb60197203a6bd744
xincao9 <xincao9@gmail.com>
2024-08-29 18:19:51 +0800

    添加账户表和用户表

--- docs/sql/data.sql
+++
@@ -1,2 +0,0 @@
-insert into user (`username`, `password`, `role`)
-values ("admin", "21232f297a57a5a743894a0e4a801fc3", 3);
--- docs/sql/schema.sql
+++ docs/sql/schema.sql
@@ -1,21 +1,34 @@
 CREATE
-DATABASE `{{.Project}}` /*!40100 DEFAULT CHARACTER SET utf8 */;
+DATABASE `pf_agent` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE
-`{{.Project}}`;
+`pf_agent`;
+
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account`
+(
+    `id`         bigint unsigned NOT NULL AUTO_INCREMENT,
+    `mobile`     char(14)  NOT NULL DEFAULT '',
+    `expire`     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `token`      char(128) NOT NULL DEFAULT '',
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
-    `id`         bigint(11) unsigned NOT NULL AUTO_INCREMENT,
-    `username`   char(32)  NOT NULL DEFAULT '',
-    `password`   char(32)  NOT NULL DEFAULT '',
-    `expire`     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `token`      char(128) NOT NULL DEFAULT '',
-    `role`       int(11) NOT NULL DEFAULT '0',
-    `created_at` timestamp NULL DEFAULT NULL,
+    `id`         bigint unsigned NOT NULL AUTO_INCREMENT,
+    `real_name`  char(32)  NOT NULL DEFAULT '',
+    `gender`     int       NOT NULL DEFAULT '0',
+    `birthday`   timestamp NULL DEFAULT NULL,
+    `address`    char(128)          DEFAULT NULL,
+    `nickname`   char(32)           DEFAULT NULL,
+    `level`      int       NOT NULL DEFAULT '1',
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` timestamp NULL DEFAULT NULL,
     `updated_at` timestamp NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `username_idx` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
